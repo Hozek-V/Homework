bb48e2829ba08b2c8f0e02e703a73c505f62b872
Hozek-V <janvystup@seznam.cz>
2024-07-07 21:22:20 +0200

    fce square"f"

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -26,3 +26,12 @@ def fig_e():
             print(" "*i + "*"*(5-2*i) + " "*i)
         else:
             print(" " * (4 - i) + "*" * (2 * i - 3) + " " * (4 - i))
+
+def fig_f():
+    for i in range(5):
+        if i<2:
+            print("*"*(i+1) + " "*(3-2*i) + "*"*(i+1))
+        elif i==2:
+            print("*"*5)
+        else:
+            print("*" * (5 - i) + " " * (2 * i - 5) + "*" * (5 - i))
