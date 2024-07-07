dcf70d21e2cc89373984fe480f9017ac07f52475
Hozek-V <janvystup@seznam.cz>
2024-07-07 20:50:56 +0200

    fce square "b"

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,4 +1,7 @@
 def fig_a():
     for i in range(5):
-        print("." +" "*i + "*"*(5-i) + ".")
-        
+        print(" "*i + "*"*(5-i))
+
+def fig_b():
+    for i in range(5):
+        print("*"*(i+1) + " "*(5-i))
